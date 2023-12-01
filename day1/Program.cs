064ed26285b03c9fab4da094fdbc1413712d7d31
Julien Blanchard <jblanchard@makemusic.com>
2023-12-01 15:07:42 +0100

    day1: wip

---
+++ day1/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string[] lines = File.ReadAllLines("input.txt");
+int[] allDigits;
+
+foreach (string line in lines)
+{
+    int first;
+    int last;
+    int[] digits;
+
+    foreach (char c in line) {
+	if(Char.IsDigit(c)) {
+            digits.Append(c);
+        }
+    }
+}
+
+Console.WriteLine(line);
